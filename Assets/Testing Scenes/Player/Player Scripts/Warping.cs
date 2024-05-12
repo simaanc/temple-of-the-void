6fee46a65c8892e631491df608877fc0e9e9b8d6
c-monast <cameron.monast@gmail.com>
2024-05-11 17:40:44 -0700

    Added timer mechanism to player warping

--- Assets/Testing Scenes/Player/Player Scripts/Warping.cs
+++ Assets/Testing Scenes/Player/Player Scripts/Warping.cs
@@ -1,22 +1,25 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TeleportOnRightClick : MonoBehaviour
 {
     public float maxTeleportDistance = 5f;
+    private float cooldown = 2f; 
+    private float lastTeleportTime = -2f; 
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(1))
+        if (Time.time - lastTeleportTime >= cooldown && Input.GetMouseButtonDown(1))
         {
             Vector3 mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z - transform.position.z));
-            mousePosition.z = transform.position.z;
+            mousePosition.z = transform.position.z; 
 
             Vector3 direction = (mousePosition - transform.position).normalized;
-
             float distanceToMouse = Vector3.Distance(transform.position, mousePosition);
             float teleportDistance = Mathf.Min(maxTeleportDistance, distanceToMouse);
 
-            transform.position = transform.position + direction * teleportDistance;
+            transform.position += direction * teleportDistance;
+            lastTeleportTime = Time.time; 
         }
     }
 }
